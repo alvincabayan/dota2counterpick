9c619edb4b3de3a15a0561ab551087bd1ddc30ab
Alvin Cabayan <alvincabayan@gmail.com>
2021-05-18 11:44:55 +1200

    [START] Initial Commit

---
+++ src/main/java/com/alvincabayan/dota2counterpick/Dota2counterpickApplication.java
@@ -0,0 +1,13 @@
+package com.alvincabayan.dota2counterpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dota2counterpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dota2counterpickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvincabayan/dota2counterpick/Dota2counterpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvincabayan.dota2counterpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dota2counterpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
